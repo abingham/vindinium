6b14a285a9af27dcd45632062cc4b0f69dd6f6e4
Austin Bingham <austin.bingham@gmail.com>
2014-02-04 21:54:24 +0100

    Fixed neighbor check in tests.

--- clojure/test/vindinium/astar_test.clj
+++ clojure/test/vindinium/astar_test.clj
@@ -6,12 +6,9 @@
   (and (or x y)
        (not (and x y))))
 
-(defn- manhattan-distance [graph [x1 y1] [x2 y2]]
+(defn- manhattan-distance [[x1 y1] [x2 y2]]
   (+ (Math/abs (- x1 x2)) 
      (Math/abs (- y1 y2))))
-
-(defn- h [graph start goal]
-  (manhattan-distance graph start goal))
 
 (defn- graph-size [g]
   {:x (if (empty? g) 0 (count (first g))),
@@ -26,22 +23,30 @@
                      (>= ny 0)
                      (< nx size-x)
                      (< ny size-y)
-                     (xor (= x nx) (= y ny)))]
+                     (xor (= x nx) (= y ny))
+                     (not= ((graph ny) nx) 1))]
       [nx ny])))
+
+(defn- neighbors? [a b]
+  (= (manhattan-distance a b) 1))
 
 (defmacro with-private-fns [[ns fns] & tests]
   "Refers private fns from ns and runs tests in context."
   `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
      ~@tests))
 
-(defn check-path [path start goal]
-  
-)
+(defn- valid-path? [path]
+  (every? #(apply neighbors? %) (partition 2 1 path)))
+
+(defn- valid-solution? [path start goal]
+  (and (= (first path) start)
+       (= (last path) goal)
+       (valid-path? path)))
 
 (def simple-graph [[0 0 0 0]
-                   [1 1 1 1]
-                   [2 2 2 2]
-                   [3 3 3 3]])
+                   [0 0 0 0]
+                   [0 0 0 0]
+                   [0 0 0 0]])
 
 ; Tests for reconstruct-path
 (with-private-fns [vindinium.astar [reconstruct-path]]
@@ -66,7 +71,7 @@
            [0 [100 100] [100 100]]
            [20 [5 5] [-5 -5]]
            [20 [-5 5] [5 -5]]]] 
-    (is (= rslt  (manhattan-distance [] start finish)))))
+    (is (= rslt  (manhattan-distance start finish)))))
 
 (deftest test-find-neighbors-top-left-corner
   (is (= (sort [[1 0] [0 1]])
@@ -80,14 +85,52 @@
   (is (= (sort (for [x (range 3) y (range 3) :when (xor (= x 1) (= y 1))] [x y]))
          (sort (find-neighbors simple-graph [1 1])))))
 
+(deftest test-find-neighbors-exclude-right
+  (let [graph [[0 1]
+               [0 0]]
+        neighbors (find-neighbors graph [0 0])]
+    (is (= neighbors [[0 1]]))))
+
+(deftest test-find-neighbors-exclude-below
+  (let [graph [[0 0]
+               [1 0]]
+        neighbors (find-neighbors graph [0 0])]
+    (is (= neighbors [[1 0]]))))
+
+(deftest test-find-neighbors-exclude-left
+  (let [graph [[1 0]
+               [0 0]]
+        neighbors (find-neighbors graph [1 0])]
+    (is (= neighbors [[1 1]]))))
+
+(deftest test-find-neighbors-exclude-above
+  (let [graph [[1 0]
+               [0 0]]
+        neighbors (find-neighbors graph [0 1])]
+    (is (= neighbors [[1 1]]))))
 
 (deftest test-astar-simple
-  (let [graph [[0 0 0 0]
+  (let [graph [[0 1 0 0]
                [0 0 0 0]
                [0 0 0 0]
                [0 0 0 0]]
-        path (a-star [0 0]
-                     [3 3]
-                     (partial manhattan-distance graph)
+        start [0 0]
+        goal [3 3]
+        path (a-star start
+                     goal
+                     manhattan-distance
                      (partial find-neighbors graph))]
-    (println path)))
+    (is (valid-solution? path start goal))))
+
+(deftest test-astar-simple
+  (let [graph [[0 0 1 1]
+               [1 0 1 1]
+               [1 0 0 0]
+               [1 1 1 0]]
+        start [0 0]
+        goal [3 3]
+        path (a-star start
+                     goal
+                     manhattan-distance
+                     (partial find-neighbors graph))]
+    (is (valid-solution? path start goal))))
