6a246b4cb05ea18f80f48b329a131de06bdc71d3
Austin Bingham <austin.bingham@gmail.com>
2014-02-04 20:56:29 +0100

    Moving graph definition stuff into test code.

--- clojure/src/vindinium/astar.clj
+++ clojure/src/vindinium/astar.clj
@@ -2,16 +2,7 @@
   (:use clojure.data.priority-map)
   (:use clojure.test))
 
-(defn- xor [x y]
-  (and (or x y)
-       (not (and x y))))
-
-(defn- manhattan-distance [graph [x1 y1] [x2 y2]]
-  (+ (Math/abs (- x1 x2)) 
-     (Math/abs (- y1 y2))))
-
-(defn- h [graph start goal]
-  (manhattan-distance graph start goal))
+;-------------------------------------------------------------------------------
 
 (defn- reconstruct-path [came-from node]
   (loop [node node
@@ -21,39 +12,27 @@
       (recur (came-from node) 
              (conj path (came-from node))))))
 
-(defn- graph-size [g]
-  {:x (if (empty? g) 0 (count (first g))),
-   :y (count g)}
-)
-
-(defn- find-neighbors [graph [x y]]
-  (let [{size-x :x, size-y :y} (graph-size graph)]
-    (for [nx (range (dec x) (+ x 2))
-          ny (range (dec y) (+ y 2))
-          :when (and (>= nx 0)
-                     (>= ny 0)
-                     (< nx size-x)
-                     (< ny size-y)
-                     (xor (= x nx) (= y ny)))]
-      [nx ny])))
-
-(defn update-sets [graph
-                   current
-                   n
-                   goal
-                   came-from
-                   g-score
-                   f-score
-                   open-set]
+(defn- update-sets [current
+                    n
+                    goal
+                    heuristic
+                    came-from
+                    g-score
+                    f-score
+                    open-set]
   "Updates the algorithm state (g-score, f-score, open-set, and
 came-from) based on a discovering a neighber N of node CURRENT."
-  (let [tentative-score (+ (g-score current) (manhattan-distance graph current n))]
+  (let [tentative-score 
+        ; NOTE: This assumes a distance of 1 from a node to any
+        ; neighbor (as defined by the user-supplied neighbor
+        ; function.)
+        (inc (g-score current))]
     (if (or (not (contains? g-score n)) 
             (< tentative-score (g-score n)))
       
       {:came-from (assoc came-from n current),
        :g-score (assoc g-score n tentative-score),
-       :f-score (assoc f-score n (+ tentative-score (h graph n goal)))
+       :f-score (assoc f-score n (+ tentative-score (heuristic n goal)))
        :open-set (conj open-set n)}
 
       {:came-from came-from, 
@@ -62,16 +41,15 @@ came-from) based on a discovering a neighber N of node CURRENT."
        :open-set open-set})))
 
 (defn- combine-states [x y]
-  ; TODO: Surely this can be done as a comprehension. Look into it.
-  {:came-from (conj (x :came-from) (y :came-from))
-   :g-score (conj (x :g-score) (y :g-score))
-   :f-score (conj (x :f-score) (y :f-score))
-   :open-set (conj (x :open-set) (y :open-set))}
-)
+  (into {} (for [key (keys x)]
+             [key (conj (x key) (y key))])))
 
-(defn a-star [graph start goal]
+(defn a-star [start 
+              goal
+              heuristic
+              find-neighbors]
   (loop [g-score (hash-map start 0) ; distance to from start to node
-         f-score (hash-map start (h graph start goal)) ; distance to node (g-score) plus heuristic distance to goal
+         f-score (hash-map start (heuristic start goal)) ; distance to node (g-score) plus heuristic distance to goal
          closed-set (hash-set)
          open-set (hash-set start)
          came-from (hash-map)]
@@ -81,8 +59,15 @@ came-from) based on a discovering a neighber N of node CURRENT."
         (let [open-set (disj open-set current)
               closed-set (conj closed-set current)
               neighbors (filter (fn [x] (not (contains? closed-set x))) 
-                                (find-neighbors graph current))
-              state-updates (map (fn [n] (update-sets graph current n goal came-from g-score f-score open-set))
+                                (find-neighbors current))
+              state-updates (map (fn [n] (update-sets current 
+                                                      n 
+                                                      goal 
+                                                      heuristic
+                                                      came-from 
+                                                      g-score 
+                                                      f-score 
+                                                      open-set))
                                  neighbors)
               new-state (reduce 'combine-states 
                                 {:came-from came-from,
@@ -95,40 +80,3 @@ came-from) based on a discovering a neighber N of node CURRENT."
                  closed-set
                  (new-state :open-set)
                  (new-state :came-from)))))))
-
-;; function A*(start,goal)
-;;     closedset := the empty set    // The set of nodes already evaluated.
-;;     openset := {start}    // The set of tentative nodes to be evaluated, initially containing the start node
-;;     came_from := the empty map    // The map of navigated nodes.
- 
-;;     g_score[start] := 0    // Cost from start along best known path.
-;;     // Estimated total cost from start to goal through y.
-;;     f_score[start] := g_score[start] + heuristic_cost_estimate(start, goal)
- 
-;;     while openset is not empty
-;;         current := the node in openset having the lowest f_score[] value
-;;         if current = goal
-;;             return reconstruct_path(came_from, goal)
- 
-;;         remove current from openset
-;;         add current to closedset
-;;         for each neighbor in neighbor_nodes(current)
-;;             if neighbor in closedset
-;;                 continue
-;;             tentative_g_score := g_score[current] + dist_between(current,neighbor)
- 
-;;             if neighbor not in openset or tentative_g_score < g_score[neighbor] 
-;;                 came_from[neighbor] := current
-;;                 g_score[neighbor] := tentative_g_score
-;;                 f_score[neighbor] := g_score[neighbor] + heuristic_cost_estimate(neighbor, goal)
-;;                 if neighbor not in openset
-;;                     add neighbor to openset
- 
-;;     return failure
- 
-;; function reconstruct_path(came_from, current_node)
-;;     if current_node in came_from
-;;         p := reconstruct_path(came_from, came_from[current_node])
-;;         return (p + current_node)
-;;     else
-;;         return current_node
--- clojure/test/vindinium/astar_test.clj
+++ clojure/test/vindinium/astar_test.clj
@@ -2,6 +2,33 @@
   (:require [clojure.test :refer :all]
             [vindinium.astar :refer :all]))
 
+(defn- xor [x y]
+  (and (or x y)
+       (not (and x y))))
+
+(defn- manhattan-distance [graph [x1 y1] [x2 y2]]
+  (+ (Math/abs (- x1 x2)) 
+     (Math/abs (- y1 y2))))
+
+(defn- h [graph start goal]
+  (manhattan-distance graph start goal))
+
+(defn- graph-size [g]
+  {:x (if (empty? g) 0 (count (first g))),
+   :y (count g)}
+)
+
+(defn- find-neighbors [graph [x y]]
+  (let [{size-x :x, size-y :y} (graph-size graph)]
+    (for [nx (range (dec x) (+ x 2))
+          ny (range (dec y) (+ y 2))
+          :when (and (>= nx 0)
+                     (>= ny 0)
+                     (< nx size-x)
+                     (< ny size-y)
+                     (xor (= x nx) (= y ny)))]
+      [nx ny])))
+
 (defmacro with-private-fns [[ns fns] & tests]
   "Refers private fns from ns and runs tests in context."
   `(let ~(reduce #(conj %1 %2 `(ns-resolve '~ns '~%2)) [] fns)
@@ -14,18 +41,8 @@
                    [2 2 2 2]
                    [3 3 3 3]])
 
-(with-private-fns [vindinium.astar [find-neighbors
-                                    manhattan-distance
-                                    reconstruct-path
-                                    xor]]
-
-  (deftest test-manhattan-distance
-    (doseq [[rslt start finish] 
-            [[2 [0 0] [1 1]]
-             [0 [100 100] [100 100]]
-             [20 [5 5] [-5 -5]]
-             [20 [-5 5] [5 -5]]]] 
-      (is (= rslt  (manhattan-distance [] start finish)))))
+; Tests for reconstruct-path
+(with-private-fns [vindinium.astar [reconstruct-path]]
 
   (deftest test-reconstruct-path
     (is (= [[1 1] [0 1] [0 0]] 
@@ -37,20 +54,38 @@
     (is (= [[2 2]]
            (reconstruct-path {[1 1] [0 1],
                               [0 1] [0 0]}
-                             [2 2]))))
+                             [2 2])))))
 
-  (deftest test-find-neighbors-top-left-corner
-    (is (= (sort [[1 0] [0 1]])
-           (sort (find-neighbors simple-graph [0 0])))))
+; sanity tests for our utility functions
 
-  (deftest test-find-neighbors-bottom-right-corner
-    (is (= (sort [[2 3] [3 2]])
-           (sort (find-neighbors simple-graph [3 3])))))
+(deftest test-manhattan-distance
+  (doseq [[rslt start finish] 
+          [[2 [0 0] [1 1]]
+           [0 [100 100] [100 100]]
+           [20 [5 5] [-5 -5]]
+           [20 [-5 5] [5 -5]]]] 
+    (is (= rslt  (manhattan-distance [] start finish)))))
 
-  (deftest test-find-neighbors-middle
-    (is (= (sort (for [x (range 3) y (range 3) :when (xor (= x 1) (= y 1))] [x y]))
-           (sort (find-neighbors simple-graph [1 1])))))
+(deftest test-find-neighbors-top-left-corner
+  (is (= (sort [[1 0] [0 1]])
+         (sort (find-neighbors simple-graph [0 0])))))
 
-)
+(deftest test-find-neighbors-bottom-right-corner
+  (is (= (sort [[2 3] [3 2]])
+         (sort (find-neighbors simple-graph [3 3])))))
+
+(deftest test-find-neighbors-middle
+  (is (= (sort (for [x (range 3) y (range 3) :when (xor (= x 1) (= y 1))] [x y]))
+         (sort (find-neighbors simple-graph [1 1])))))
 
 
+(deftest test-astar-simple
+  (let [graph [[0 0 0 0]
+               [0 0 0 0]
+               [0 0 0 0]
+               [0 0 0 0]]
+        path (a-star [0 0]
+                     [3 3]
+                     (partial manhattan-distance graph)
+                     (partial find-neighbors graph))]
+    (println path)))
